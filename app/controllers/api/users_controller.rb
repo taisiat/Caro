34ee9d6e4cee857f864a4f8ff3f0e4497a04b86e
taisiat <113080884+taisiat@users.noreply.github.com>
2023-04-21 15:25:37 -0700

    Add user auth backend logic in Api::UsersController

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,22 @@
 class Api::UsersController < ApplicationController
+
+  wrap_parameters include: User.attribute_names + ['password']
+
   def create
+      @user = User.new(user_params);
+      if @user.save
+        login!(@user)
+        render json: { user: @user }
+      else
+        render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+      end
+  end
+
+  private
+
+  def user_params
+    params
+      .require(:user)
+      .permit(:first_name, :last_name, :approved_to_drive, :is_superhost, :is_clean_certified, :email, :phone_number, :password)
   end
 end
