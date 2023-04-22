86e514bbcf4d61a55a88844e29c55175d027bc00
taisiat <113080884+taisiat@users.noreply.github.com>
2023-04-22 13:43:23 -0700

    Add login dropdown

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
+import SignupFormPage from "./components/SignupFormPage";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <>
+      <Navigation />
       <Switch>
         <Route path="/login">
           <LoginFormPage />
+        </Route>
+        <Route path="/signup">
+          <SignupFormPage />
         </Route>
       </Switch>
     </>
---
+++ frontend/src/components/Navigation/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ProfileButton from "./ProfileButton";
+// import LoginFormModal from "../LoginFormModal";
+import "./Navigation.css";
+
+function Navigation() {
+  const sessionUser = useSelector((state) => state.session.user);
+
+  let sessionLinks;
+  if (sessionUser) {
+    sessionLinks = <ProfileButton user={sessionUser} />;
+  } else {
+    sessionLinks = (
+      <>
+        {/* <LoginFormModal /> */}
+        <NavLink to="/login">Log in</NavLink>
+        <NavLink to="/signup">Sign up</NavLink>
+      </>
+    );
+  }
+
+  return (
+    <ul>
+      <li>
+        <NavLink exact to="/">
+          Home
+        </NavLink>
+        {sessionLinks}
+      </li>
+    </ul>
+  );
+}
+
+export default Navigation;
